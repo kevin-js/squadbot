a1e18a29c9fc05dc614d58d876831831ff08a3f6
Kevin Guh <kevinjguh@yahoo.com>
2016-03-30 15:19:04 -0700

    suh dude

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+// Node modules
 var restify = require('restify');
 var builder = require('botbuilder');
 
+// application vars
+var phrases = ['squaaaad', 'suh dude', 'dab for Bill Gates', 'it\'s lit'];
+var index = 0;
+
+// endpoints
 var bot = new builder.BotConnectorBot({appId: 'Squad', appSecret: '91606fa3e08e456a9b22c244c2589cbf'});
 bot.add('/', function(session) {
-	session.send('Hello World');
+	setTimeout(function() {
+		session.send(phrases[index % phrases.length]);
+		index++;
+	}, 10000);
+	
 });
 
+// run server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function() {
